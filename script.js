697be3c37ee436dc7eacf4d2e28007cdbe65a659
HyriSoni <hyrisoni@gmail.com>
2025-06-07 04:45:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// About Modal
+const aboutModal = document.getElementById('aboutModal');
+const aboutBtn = document.getElementById('aboutBtn');
+const closeAboutModalBtn = document.getElementById('closeAboutModalBtn');
+
+aboutBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  aboutModal.classList.add('show');
+});
+
+closeAboutModalBtn.addEventListener('click', () => {
+  aboutModal.classList.remove('show');
+});
